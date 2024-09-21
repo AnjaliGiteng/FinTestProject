e4505eeb644026a3107d05fafbafd516551cdc39
KAnjaliChauhan <166163340+KAnjaliChauhan@users.noreply.github.com>
2024-09-20 23:13:41 -0700

    updating headless mode

--- utils/setup_teardown.py
+++ utils/setup_teardown.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import unittest
 
 
 class BaseTest(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(10)
 
     def tearDown(self):
